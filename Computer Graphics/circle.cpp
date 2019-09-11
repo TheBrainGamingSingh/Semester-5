af8162d7e3fb7be9617555935e0c829c7ecfa763
Manpreet <itsmanpreetsinghjuneja@gmail.com>
2019-09-11 10:53:41 +0000

    Added Computer Graphics

---
+++ Computer Graphics/circle.cpp
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<graphics.h>
+
+void circle1(float H,float K,float r)
+{
+  float h = (float)getwindowheight();
+  float w = (float)getwindowwidth();
+  float x = 0;
+  float y = r;
+  float p;
+
+  while(x <= y)
+  {
+
+  //  putpixel(H + x,y + K,WHITE);
+    putpixel(H + x + w/2,(h/2) - (y + K),WHITE); //x,y
+    putpixel(H + y + w/2,(h/2) - (x + K),WHITE); //y,x
+
+    putpixel(H + (-1 * x) + w/2,(h/2) - (y + K),WHITE); //-x,y
+    putpixel(H + (-1 * y) + w/2,(h/2) - (x + K),WHITE); //-y,x
+
+    putpixel(H + (-1 * x) + w/2,(h/2) - ((-1 * y) + K),WHITE); //-x,-y
+    putpixel(H + (-1 * y) + w/2,(h/2) - ((-1 * x) + K),WHITE); //-y,-x
+
+    putpixel(H + (x) + w/2,(h/2) - ((-1 * y) + K),WHITE); //x,-y
+    putpixel(H + (y) + w/2,(h/2) - ((-1 * x) + K),WHITE); //y,-x
+    delay(20);
+
+    p = (x + 1) * (x + 1) + (y - 0.5) * (y - 0.5) - r * r ;
+    if(p >= 0)
+     y--;
+
+    x++;
+   }
+
+}
+//Function for finding absiolute value
+int absi (int n)
+{
+    return ( (n>0) ? n : ( n * (-1)));
+}
+
+//DDA Function for line generation
+void DDA(float X0, float Y0, float X1, float Y1)
+{
+    float h = (float)getwindowheight();
+    float w = (float)getwindowwidth();
+
+    // calculate dx & dy
+    float dx = X1 - X0;
+    float dy = Y1 - Y0;
+
+    // calculate steps required for generating pixels
+    int steps = absi(dx) > absi(dy) ? absi(dx) : absi(dy);
+
+    // calculate increment in x & y for each steps
+    float Xinc = dx / (float) steps;
+    float Yinc = dy / (float) steps;
+
+    // Put pixel for each step
+    float X = X0;
+    float Y = Y0;
+    for (int i = 0; i <= steps; i++)
+    {
+        putpixel (X+w/2,(h/2) - Y,WHITE);
+        //delay(10);   // put pixel at (X,Y)
+        X += Xinc;                          // increment in x at each step
+        Y += Yinc;                          // increment in y at each step
+    }
+}
+
+// Driver program
+int main()
+{
+    int gd = DETECT, gm;
+
+    // Initialize graphics function
+    initgraph (&gd, &gm, "");
+
+    float h = (float)getwindowheight();
+    float w = (float)getwindowwidth();
+
+    DDA(-w/2,0, w/2, 0);
+    DDA(0,h/2,0,-h/2);
+
+    //Input values
+    //DDA(0,0,w,w);
+    circle1(0,0,100);
+    circle1(0,0,75);
+    circle1(0,0,50);
+    circle1(0,0,25);
+    getch();
+    return 0;
+}
---
+++ Web Tech/Classwork/basics.java
@@ -0,0 +1,19 @@
+public class basics
+{
+  public static void main(String args[])
+  {
+    int x = 0x10;
+    char c = 88;
+    char d = 'Y';
+    boolean a = true;
+    if(a == true)
+      {
+        System.out.println("Yes");
+        System.out.println(a);
+      }
+    boolean y,z = true;
+    y = z;
+    System.out.println(y + " " + z);
+    System.out.println(x + " " + c + " " + d);
+  }
+}
