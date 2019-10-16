da0c17525af99da98022456d247dc541368f0219
TheBrainGamingSingh <itsmanpreetsinghjuneja@gmail.com>
2019-10-16 10:45:22 +0000

    added prolog

--- AI/prolog/family_tree.pl
+++ AI/prolog/family_tree.pl
@@ -1,11 +1,23 @@
+:- discontiguous male/1.
+:- discontiguous female/1.
 male(manpreet).
+female(ekjot).
 female(gagan).
 male(jatinder).
-parent_of(gagan,manpreet).
-parent_of(jatinder,manpreet).
+male(dalip).
+female(surinder).
+parent(dalip,jatinder).
+parent(surinder,jatinder).
+parent(gagan,manpreet).
+parent(jatinder,manpreet).
+parent(gagan,ekjot).
+parent(jatinder,ekjot).
 
 
-father_of(X,Y):- male(X),parent_of(X,Y).
-mother_of(X,Y):- female(X),parent_of(X,Y).
-ancestor_of(X,Y):- parent_of(X,Y).
-ancestor_of(X,Y):- parent_of(X,Z),ancestor_of(Z,Y).
+father(X,Y):- male(X),parent(X,Y).
+mother(X,Y):- female(X),parent(X,Y).
+sibling(X,Y):- parent(Z,X),parent(Z,Y), X \= Y.
+son(X,Y):- male(X),parent(Y,X).
+daughter(X,Y):- female(X),parent(Y,X).
+grandparent(X,Y):- parent(X,Z),parent(Z,Y).
+grandfather(X,Y):- male(X),grandparent(X,Y).
