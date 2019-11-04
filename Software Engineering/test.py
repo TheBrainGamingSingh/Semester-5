9daab4d3778a471cb9fd9f3cd7e2177ffad4c627
TheBrainGamingSingh <itsmanpreetsinghjuneja@gmail.com>
2019-11-04 20:21:35 +0000

    edited files

--- Software Engineering/test.py
+++ Software Engineering/test.py
@@ -1,14 +1,4 @@
-print('Hello World')
-import matplotlib.pyplot as plt
-x = [i for i in range(10)]
-print(x)
-plt.hist(x)
-
-y = [i**2 for i in range(10)]
-plt.hist(x,y)
-plt.bar(x,y)
-plt.scatter(x,y)
-plt.hist([x,y])
-#I edited using Windows
-#I edited using Ubuntu 19.04
-#Testing git
+i = 10
+while i < 18:
+    i = i + 2
+print(i)
